b02638cf2743499d9c3a5ec3743d3aba083c729c
Nazario A. Ayala <nazario@niztech.com>
2019-03-04 22:19:27 -0800

    Added some enums

---
+++ DerbyScoreBoardSwift/DerbyConstants.swift
@@ -0,0 +1,27 @@
+//
+//  DerbyConstants.swift
+//  DSBv2
+//
+//  Created by Nazario Ayala on 3/4/19.
+//  Copyright © 2019 Nazario Ayala. All rights reserved.
+//
+
+import Foundation
+
+enum ModalMode: Equatable {
+    case Edit;
+    case Add;
+}
+
+enum Clock: Equatable {
+    case Bout;
+    case Jam;
+    case Period;
+}
+
+enum TeamDesignation: Equatable {
+    case None;
+    case Home;
+    case Visitor;
+}
+
